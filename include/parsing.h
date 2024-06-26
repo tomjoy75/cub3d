490f3ad802e2de2cde9c62ac69dff5351d8a50ad
tomjoy75 <tomjoy75@gmail.com>
2024-06-27 00:22:47 +0200

    Calculate the size of the map for allocation

--- include/parsing.h
+++ include/parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjoyeux <tjoyeux@student.42.fr>            +#+  +:+       +#+        */
+/*   By: joyeux <joyeux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 18:05:17 by tjoyeux           #+#    #+#             */
-/*   Updated: 2024/06/25 15:02:24 by tjoyeux          ###   ########.fr       */
+/*   Updated: 2024/06/27 00:16:53 by joyeux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int cb_check_args(int argc, char** argv);
 t_list	*cb_build_linked_list(int fd);
 //Utils
 void	cb_error_msg(char *msg);
+void	replace_newline_by_null(char *str);
 //inline int	is_whitespace(char c);
 
 
 //Check lines
 int	is_valid_map_line(char *str);
 int is_valid_element_line(char *str);
+int is_space_line(char *str);
 
 #endif
