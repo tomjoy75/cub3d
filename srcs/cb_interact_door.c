4e72a5b3f0253fdc8192a2fb180584a540a48cda
jerperez42 <jerperez@student.42.fr>
2024-07-17 19:32:25 +0200

    fix door double fix door clip

--- srcs/cb_interact_door.c
+++ srcs/cb_interact_door.c
@@ -17,10 +17,21 @@
 
 void	_change_tile(t_data *data, double *xy, double *dxy, int tile)
 {
+	int		xy_player[2];
+	int		xy_collision[2];
 	int		xy_map[2];
 
+	xy_player[0] = (int)floorf(xy[0]);
+	xy_player[1] = (int)floorf(xy[1]);
+	xy_collision[0] = (int)floorf(\
+		xy[0] + CB_COLLISION_TOL * xy[CB_PLAYER_COS_INDEX]);
+	xy_collision[1] = (int)floorf(\
+		xy[1] + CB_COLLISION_TOL * xy[CB_PLAYER_SIN_INDEX]);
 	xy_map[0] = (int)floorf(xy[0] + dxy[0]);
 	xy_map[1] = (int)floorf(xy[1] + dxy[1]);
+	if ((xy_player[0] == xy_map[0] && xy_player[1] == xy_map[1]) || \
+			(xy_collision[0] == xy_map[0] && xy_collision[1] == xy_map[1]))
+		return ;
 	data->map->cells[xy_map[1]][xy_map[0]] = tile;
 }
 
@@ -31,19 +42,14 @@ void	cb_interact_door(t_data *data)
 {
 	int		tile;
 	double	dxy[2];
-	double	*xy;
+	double	*xydcs;
 
-	xy = data->player_xydcs;
-	dxy[0] = 0.0;
-	dxy[1] = 0.0;
-	tile = cb_interacted_tile(data, xy, dxy);
-	if (CB_TILE_DOOR == tile || CB_TILE_DOOR_OPEN == tile)
-		return ;
-	dxy[0] = CB_DOOR_OPEN_DIST * xy[CB_PLAYER_COS_INDEX];
-	dxy[1] = CB_DOOR_OPEN_DIST * xy[CB_PLAYER_SIN_INDEX];
-	tile = cb_interacted_tile(data, xy, dxy);
+	xydcs = data->player_xydcs;
+	dxy[0] = CB_DOOR_OPEN_DIST * xydcs[CB_PLAYER_COS_INDEX];
+	dxy[1] = CB_DOOR_OPEN_DIST * xydcs[CB_PLAYER_SIN_INDEX];
+	tile = cb_interacted_tile(data, xydcs, dxy);
 	if (CB_TILE_DOOR == tile)
-		_change_tile(data, xy, dxy, CB_TILE_DOOR_OPEN);
+		_change_tile(data, xydcs, dxy, CB_TILE_DOOR_OPEN);
 	if (CB_TILE_DOOR_OPEN == tile)
-		_change_tile(data, xy, dxy, CB_TILE_DOOR);
+		_change_tile(data, xydcs, dxy, CB_TILE_DOOR);
 }
